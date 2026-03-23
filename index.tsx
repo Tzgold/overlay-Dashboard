0cdfdf525a211d146d6e1da6ee77265e44fd786c
Tequam z <tequamzgold@gmail.com>
2026-03-23 16:47:36 +0300

    favicon

--- index.tsx
+++ index.tsx
@@ -2,11 +2,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import overlayLogo from './overlay.png';
+
+const ensureFavicon = (href: string) => {
+  const existing = document.querySelector('link[rel="icon"]') as HTMLLinkElement | null;
+  const link = existing ?? document.createElement('link');
+  link.rel = 'icon';
+  link.type = 'image/png';
+  link.href = href;
+  if (!existing) document.head.appendChild(link);
+};
 
 const rootElement = document.getElementById('root');
 if (!rootElement) throw new Error("Could not find root element");
 
 const root = ReactDOM.createRoot(rootElement);
+
+ensureFavicon(overlayLogo);
+
 root.render(
   <React.StrictMode>
     <App />
